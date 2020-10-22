cb8de7cf499b9ca1803b42c843e421d1d023090f
Nac Icandid <athikom.rum@gmail.com>
2020-10-22 10:48:50 +0700

    update README.md

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-// import * as serviceWorker from './serviceWorker'
+import * as serviceWorker from './serviceWorker'
 import './css/normalize.css'
 import './css/app.css'
 
-if (process.env.NODE_ENV === 'development') {
-	const { worker } = require('./mocks/browser')
-	worker.start()
-}
+// if (process.env.NODE_ENV === 'development') {
+// 	const { worker } = require('./mocks/browser')
+// 	worker.start()
+// }
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -17,4 +17,4 @@ ReactDOM.render(
 	document.getElementById('root')
 )
 
-// serviceWorker.unregister()
+serviceWorker.unregister()
